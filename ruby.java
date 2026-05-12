29747bcdc5dcc334e00560e306148bd04a8a7708
ruby0909 <rs1929469@gmail.com>
2026-05-12 18:24:06 +0530

    Create ruby.java

---
+++ ruby.java
@@ -0,0 +1,33 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class ruby {
+    public static void main(String args[]) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mango","root", "root");
+
+            Statement st = conn.createStatement();
+
+            String query = "create table new13(id int, name varchar(20))";
+            st.executeUpdate(query);
+
+            st.executeUpdate("insert into new13 values(1,'ruby singh')");
+            st.executeUpdate("insert into new13 values(2,'muskan')");
+            st.executeUpdate("insert into new13 values(3,'nikshay')");
+            st.executeUpdate("insert into new13 values(4,'naman')");
+            st.executeUpdate("insert into new13 values(5,'roshni')");
+            System.out.println("Connection built successfully");
+
+            st.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Connection failed");
+            System.out.println(e);
+        }
+    }
+}
