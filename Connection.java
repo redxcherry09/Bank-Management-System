45067c690bb3bd11f5a6db9270ac5ab0c029ce50
ruby0909 <rs1929469@gmail.com>
2026-05-12 18:19:33 +0530

    Add database connection class for MySQL

---
+++ Connection.java
@@ -0,0 +1,18 @@
+package Connection;
+
+import java.sql.*;
+
+public class Conn {
+    Connection connection;
+    Statement statement;
+    public Conn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","root");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
